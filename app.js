2afedbacc3fbcaa67f658536271cbd209881b437
Rabin124 <rabinady123@gmail.com>
2024-12-01 13:55:17 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get("/", (req, res)=>{
+  // console.log(req);
+  // res.send("Hello, world!");
+  res.json({
+    message: "This is a home page"
+  })
+})
+
+app.get('/about',(req, res) => {
+  res.json({
+    message: "This is a about page"
+  })
+})
+
+app.listen(3000, ()=>{
+  console.log('listening on port 3000');
+});
